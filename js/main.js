17b380beb683fb1de175450bbbacf54f2909cda5
LWestTech <91633686+LWestTech@users.noreply.github.com>
2022-03-15 19:39:55 -0400

    added terminal beginning

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@ magneticButtonsBox.forEach(magneticButtonsBox => {
         const position = magneticButtonsBox.getBoundingClientRect();
 
         const x = e.pageX - position.left - position.width / 2;
-        const y = e.pageY - position.top - position.height / 2;
+        const y = e.pageY - window.scrollY - position.top - position.height / 2;
 
-        magneticButtonsBox.style.transform = `translate(${x * 0.5}px, ${y * 0.5}px)`;
+        magneticButtonsBox.style.transform = `translate(${x * 0.3}px, ${y * 0.3}px)`;
     })
     magneticButtonsBox.addEventListener('mouseout', (e) =>{
         const position = magneticButtonsBox.getBoundingClientRect();
@@ -18,3 +18,22 @@ magneticButtonsBox.forEach(magneticButtonsBox => {
         magneticButtonsBox.style.transform = `translate(0px, 0px)`;
     })
 })
+
+const controller = new ScrollMagic.Controller();
+
+const consoleReveal = new ScrollMagic.Scene({
+    triggerElement: '.console',
+})
+.setClassToggle('.console', 'fadeIn')
+// .addIndicators()
+.addTo(controller);
+
+// const textScene = new ScrollMagic.Scene({
+//     triggerElement: '.console',
+//     duration: 300,
+//     triggerHook: 0
+// })
+// .addIndicators()
+// .setPin('.console')
+// .addTo(controller);
+
